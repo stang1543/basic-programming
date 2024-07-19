1d2296bd817f6c22e93d4c6e64b8a094d46b03c7
nexthanwat <nexpair@gmail.com>
2024-07-19 11:48:38 +0700

    การบ้าน

--- Ep 1/Index.ts
+++ Ep 1/Index.ts
@@ -1,1 +1,51 @@
-const FullName:  string = 'ThaNaWat ChanTaMas'
+const FullName:  string = 'thaNaWat ChanTaMas'
+var x = 20
+var = 10 
+var hello = 5
+
+var msg = 'hello' // singLe
+var msg = "can't" // double
+x = 'สวัสดี'
+x = 'hello'
+x = true
+
+var isPerson = false
+
+console.log(x)
+console.log(msg)
+console.log(isPerson)
+
+const firstName: string = 'thaNaWat chanTaMas'
+let y: number = 10
+let iWantToGoHome = true
+ y = 20 
+
+ console.log(firstName)
+ console.log(y)
+ console.log(goHome)
+
+ const first_name: string = 'thaNaWat'
+ let y: number = 10
+ let i_wat_to_go_home: boolean = true
+
+ y = 20
+
+ console.log(first_name)
+ console.log(y)
+ console.log(i_wat_to_go_home)
+
+ let x: number = 10
+ let y: string = '10'
+ === // เท่ากับหรือเปล่า
+ > // มากกว่า
+ < // น้อยกว่า
+ >= // มากกว่าหรือเท่ากับ
+ <= // น้อยกว่าหรือเท่ากับ
+ ！== // ไม่เท่ากับ
+
+ const a: number = 10
+ const b: number = 20
+
+ console.log(a === b)
+ console.log(a > b)
+ console.log(a <= b)
