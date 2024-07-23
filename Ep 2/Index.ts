25f675dc5581258708604cc9f2eb9b71008ca4ed
Penpitcha03 <645051200287@mail.rmutk.ac.th.com>
2024-07-23 11:50:21 +0700

    เรียน function

---
+++ Ep 2/Index.ts
@@ -0,0 +1,16 @@
+function grade(homework: number, midterm: number, final: number) {
+    let score: number = homework + midterm + final
+    if (score < 50) {
+        return 'เกรด F'
+    } else if (score < 60) {
+        return 'เกรด D'
+    } else if (score < 70) {
+        return 'เกรด C'
+    } else if (score < 80) {
+        return 'เกรด B'
+    } else if (score > 90) {
+        return 'เกรด A'
+    }
+}
+
+console.log(grade(10, 40, 50))
